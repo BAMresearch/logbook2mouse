16eb8c39baee6e0635061d5b00cbd964a708da69
ahoerman <anja.hoermann@bam.de>
2024-12-03 14:23:05 +0100

    add function for repeated measurements at a configuration
    with default repetitions as used previously

--- logbook2mouse/measure_config.py
+++ logbook2mouse/measure_config.py
@@ -72,3 +72,38 @@ def measure_dataset(
     )
     epics.caput("source_cu:shutter", "Closed")
 
+
+def measure_at_config(
+    config_no: int, entry, required_pvs, dEiger_connection, repetitions=None
+):
+    """Measure with the default settings for each configuration."""
+    config_dict = moveto_config(
+        required_pvs,
+        config_path=Path("/mnt/vsi-db/Measurements/SAXS002/data/configurations"),
+        config_no=config_no,
+    )
+
+    if repetitions is None:
+        if config_no in [117, 127]:
+            repetitions = 16
+        elif config_no in [115, 125]:
+            repetitions = 10
+        elif config_no in [113, 123]:
+            repetitions = 5
+        else:
+            repetitions = 1
+
+    scan_counter = 0
+    work_directory = filemanagement.work_directory(entry)
+    ymd = entry.date.strftime("%Y%m%d")
+    for i in range(repetitions):
+        next_measurement_no = filemanagement.scan_counter_next(
+            scan_counter, work_directory, entry
+        )
+        store_location = work_directory / f"{ymd}_{next_measurement_no}"
+        measure_dataset(
+            entry,
+            dEiger_connection,
+            store_location=store_location,
+            bsr=config_dict["bsr"],
+        )
