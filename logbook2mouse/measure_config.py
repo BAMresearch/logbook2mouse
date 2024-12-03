b54cc59afb6be273493ac06b42543ce092e1ebdd
ahoerman <anja.hoermann@bam.de>
2024-12-03 14:22:00 +0100

    add functions for beam profile and regular measurement
    to do: don't hard code the pv addresses

--- logbook2mouse/measure_config.py
+++ logbook2mouse/measure_config.py
@@ -27,3 +27,48 @@ def moveto_config(
         if name == "bsr":
             bsr = position
     return {"bsr": position}
+
+
+def measure_profile(
+    entry,
+    store_location,
+    dEiger_connection,
+    mode="blank",
+    duration: int = 20,  # add functionality to determine time needed later
+):
+    if mode == "blank":
+        epics.caput("mc0:ysam", entry.blankpositiony)
+        epics.caput("mc0:zsam", entry.blankpositionz)
+        beamprofilepath = store_location / "beam_profile"
+        if not os.path.exists(beamprofilepath):
+            mkdir(beamprofilepath)
+    else:
+        epics.caput("mc0:ysam", entry.positiony)
+        epics.caput("mc0:zsam", entry.positionz)
+        beamprofilepath = store_location / "beam_profile_through_sample"
+        if not os.path.exists(beamprofilepath):
+            mkdir(beamprofilepath)
+    epics.caput("source_cu:shutter", "Open")
+    detector.measurement(
+        dEiger_connection, duration=duration, store_location=store_location
+    )
+    epics.caput("source_cu:shutter", "Closed")
+
+
+def measure_dataset(
+    entry, dEiger_connection, store_location: Path, bsr: float, duration: int = 600
+):
+    epics.caput("ims:bsr", 270)
+    for mode in ["blank", "sample"]:
+        measure_profile(
+            entry, store_location, dEiger_connection, mode=mode, duration=20
+        )
+    epics.caput("mc0:ysam", entry.positiony)
+    epics.caput("mc0:zsam", entry.positionz)
+    epics.caput("ims:bsr", bsr)
+    epics.caput("source_cu:shutter", "Open")
+    detector.measurement(
+        dEiger_connection, duration=duration, store_location=store_location
+    )
+    epics.caput("source_cu:shutter", "Closed")
+
