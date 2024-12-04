1f43df61a5cae37604a45763588bbf1a2220a52a
ahoerman <anja.hoermann@bam.de>
2024-12-04 18:57:47 +0100

    add protocol with minimal syntax for standard measurements
    movement of motors is fully determined by the logbook entry and
    configuration number
    (to be tested)

---
+++ protocols/20241204_standard_configuration_minimal.py
@@ -0,0 +1,22 @@
+# 20241204_standard_configuration_minimal.py
+
+from logbook2mouse.measure_config import measure_at_config
+
+logging.info(f'Starting entry for logbook row {entry.row_index}, sampleID: {entry.sampleid}.')
+
+# Configurations to measure
+if "configuration" in entry.additional_parameters.keys():
+    configuration = entry.additional_parameters['configuration']
+# else:  # specify configuration manually here
+
+# Simulate starting the measurement
+print(f"Starting measurement for sample {entry.sampleid} in configuration {configuration}.")
+
+# blank and transmission measurements are included
+measure_at_config(config_no = configuration,
+                  entry = entry,
+                  required_pvs = required_pvs,
+                  dEiger_connection = eiger,
+                  # duration=60,  # default: 600
+                  # repetitions=16  # default: determined by config number
+                  )
