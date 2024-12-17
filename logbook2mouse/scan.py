23eb30a45d962699aa1e4c50caeef8633e7a2778
ahoerman <anja.hoermann@bam.de>
2024-12-17 12:17:27 +0100

    Add first draft of scan routine based on the image processing ioc
    writing the data to current_scan.csv read by the dashboard

---
+++ logbook2mouse/scan.py
@@ -0,0 +1,48 @@
+from logbook2mouse.measure_config import move_motor, measure_profile
+from logbook2mouse.experiment import get_address
+import epics
+from csv import DictWriter
+from pathlib import Path
+
+def scan(motorname, scan_start, scan_end, npoints,
+         seconds,
+         experiment, entry, store_location):
+    """Scan and record transmission relative to the current position."""
+    motor_addr = get_address(experiment, motorname)
+    prefix = motor_addr.rstrip(f":{motorname}")
+    current_pos = epics.caget(motor_addr)
+
+    # measure direct beam as a reference
+    measure_profile(entry, store_location, experiment,
+                    mode="blank",
+                    duration=seconds)
+
+    # write values to where the dashboard can see them
+    scan_csv = Path("/home/ws8665-epics/scan-using-epics-ioc/") / "current_scan.csv"
+    with open(scan_csv, "w", newline = "") as current_file:
+        writer = DictWriter(current_file,
+                            fieldnames = ["point", "value"])
+        writer.writerow({"point": motor, "value": "ratio"})
+
+    # get pv address of transmission / image ratio
+    transmission_addr = get_address(experiment, "ratio")
+
+    for point in np.linspace(current_pos + scan_start,
+                             current_pos + scan_end,
+                             npoints):
+
+        move_motor(motorname, prefix = prefix, point)
+
+        measure_profile(entry, store_location, experiment,
+                        mode="scan",
+                        duration=seconds)
+
+        transmission = epics.caget(transmission_addr)
+        with open(scan_csv, "a", newline = "") as current_file:
+            writer = DictWriter(current_file,
+                                fieldnames = ["point", "value"])
+            writer.writerow({"point": point, "value": transmission})
+
+    # move back to initial position
+    move_motor(motorname, prefix = prefix, current_pos)
+    return 0
