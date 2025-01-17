8c40e8d944b3209b2af5c0e6fec8b3cadaa43dfc
ahoerman <anja.hoermann@bam.de>
2025-01-17 19:44:14 +0100

    remove duplicate ymd in directory structure

--- protocols/20250109_alignment.py
+++ protocols/20250109_alignment.py
@@ -25,7 +25,7 @@ samplewidth = entry.additional_parameters.get('samplewidth', samplelength)
 
 # define where to save scans
 ymd = entry.date.strftime("%Y%m%d")
-scan_dir = Path(work_directory(entry)) / ymd / f"{ymd}_{entry.batchnum}_{0}" / "scans"
+scan_dir = Path(work_directory(entry)) / f"{ymd}_{entry.batchnum}_{0}" / "scans"
 os.makedirs(scan_dir / "scan_0", exist_ok=True)
 
 move_to_sampleposition(experiment, entry.sampleposition)
