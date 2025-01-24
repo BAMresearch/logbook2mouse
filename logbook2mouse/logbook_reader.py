f2f5946a89c6b72f565eb4f065af08cec4c76c4b
ahoerman <anja.hoermann@bam.de>
2025-01-24 15:25:04 +0100

    make ymd optional, generate if needed

--- logbook2mouse/logbook_reader.py
+++ logbook2mouse/logbook_reader.py
@@ -49,10 +49,11 @@ class Logbook2MouseEntry:
     project: Optional[ProjectInfo] = attrs.field(default=None)
     sample: Optional[Sample] = attrs.field(default=None)
     sampleposition: Optional[Dict[str, float]] = attrs.field(default=None)
-    ymd: str = attrs.field(init=False)
+    ymd: Optional[str] = attrs.field(default = None)
 
     def __attrs_post_init__(self):
-        self.ymd = self.date.strftime("%Y%m%d")
+        if self.ymd is None:
+            self.ymd = self.date.strftime("%Y%m%d")
 
     @classmethod
     def from_series(cls, series: pd.Series):
