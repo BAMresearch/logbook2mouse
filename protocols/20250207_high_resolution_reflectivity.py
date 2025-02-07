b60dd4dc2f6748fc552fa8c6b86640eb892d0ab6
ahoerman <anja.hoermann@bam.de>
2025-02-07 10:29:56 +0100

    adapt reflectivity script for 0.0005° step size (high-resolution)

---
+++ protocols/20250207_high_resolution_reflectivity.py
@@ -0,0 +1,39 @@
+# 20250116_reflectivity.py
+
+from logbook2mouse.scan import scan
+from logbook2mouse.measure_config import (
+    move_motor, move_motor_fromconfig, moveto_config,
+    measure_at_config)
+from logbook2mouse.file_management import work_directory, scan_counter_next
+from logbook2mouse.experiment import get_address
+from pathlib import Path
+import epics
+
+configuration = entry.additional_parameters.get('configuration', None)
+if configuration is not None:
+    moveto_config(experiment.required_pvs,
+                  config_no = configuration)
+
+ymd = entry.date.strftime("%Y%m%d")
+wd = Path(work_directory(entry))
+aligned_data =  wd / f"{ymd}_{entry.batchnum}_{1}"
+scan_dir = wd / f"{ymd}_{entry.batchnum}_{scan_counter_next(0, wd, entry)}"
+
+# move to aligned position
+for motor in ["ysam", "zheavy", "yawgi", "rollgi", "pitchgi"]:
+    motorname, motorpos = move_motor_fromconfig(motor, imcrawfile=aligned_data/"im_craw.nxs",
+                                                prefix=get_address(experiment, motor).split(":")[0]
+                                                )
+    entry.sampleposition[motor] = motorpos
+
+incident_angle_zero = epics.caget(get_address(experiment, "pitchgi"))
+
+
+# scan
+scan("pitchgi", -0.3, -0.1, 401, # 401 points - 0.0005° step size (smallest we can do)
+     1, # second
+     experiment, entry.sampleposition,
+     scan_dir
+     )
+
+
