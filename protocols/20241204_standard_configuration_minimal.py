000588089baa2602ab9526737ee2e095b9889800
Anja F. Hörmann <ws8665-epics@WS8665.zit.bam.de>
2025-02-14 13:11:08 +0100

    fix standard configuration protocol (checked out from grazing incidence branch)

--- protocols/20241204_standard_configuration_minimal.py
+++ protocols/20241204_standard_configuration_minimal.py
@@ -5,8 +5,11 @@ from logbook2mouse.measure_config import measure_at_config
 logging.info(f'Starting entry for logbook row {entry.row_index}, sampleID: {entry.sampleid}.')
 
 # Configurations to measure
-if "configuration" in entry.additional_parameters.keys():
-    configuration = entry.additional_parameters['configuration']
+configuration = int(entry.additional_parameters.get('configuration', 125))
+duration = int(float(entry.additional_parameters.get('duration', 600)))
+repetitions = entry.additional_parameters.get('repetitions', None)
+if repetitions is not None:
+    repetitions = int(float(repetitions))
 # else:  # specify configuration manually here
 
 # Simulate starting the measurement
@@ -16,6 +19,6 @@ print(f"Starting measurement for sample {entry.proposal}-{entry.sampleid} in con
 measure_at_config(config_no = configuration,
                   entry = entry,
                   experiment = experiment,
-                  duration=10,  # default: 600
-                  # repetitions=16  # default: determined by config number
+                  duration=duration,  # default: 600
+                  repetitions=repetitions  # default: determined by config number
                   )
