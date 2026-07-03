d96fd8177aa24f5a3e5e3db3b0b21e33acf2c56a
Anja F. Hörmann <ws8665-epics@WS8665.zit.bam.de>
2026-07-03 16:57:15 +0200

    setup protocol: switch to GI stage motor PVs
    standard measurements use mouse_bluesky now

--- protocols/setup.py
+++ protocols/setup.py
@@ -1,6 +1,7 @@
 # this code gets run at the start of every script (not at the start of every entry, but the whole script)
-# use this code to check PVs, generators, etc. 
+# use this code to check PVs, generators, etc.
 import logging
+
 logger = logging.getLogger("measurement")
 logger.setLevel(logging.INFO)
 from pathlib import Path
@@ -15,37 +16,38 @@ from logbook2mouse.experiment import ExperimentVariables
 
 # Required Process Variables (PVs) for this protocol. These will be validated before execution.
 required_pvs = [
-    'newport1:ysam',
-    'newport1:zsam',
-    # 'mc0:zheavy',
-    # 'mc0:yawgi',
-    # 'mc0:rollgi',
-    # 'mc0:pitchgi',
-    'ims:detx',
-    'ims:dety',
-    'ims:detz',
-    'ims:s1bot',
-    'ims:s1top',
-    'ims:s1hl',
-    'ims:s1hr',
-    'ims:s2bot',
-    'ims:s2top',
-    'ims:s2hl',
-    'ims:s2hr',
-    'ims:s3bot',
-    'ims:s3top',
-    'ims:s3hl',
-    'ims:s3hr',
-    'ims:bsr',
-    'ims:bsz',
-    'ims:dual',
-    'source_cu:shutter',  # uncomment once the source is repaired
-    'source_mo:shutter',
-    'eiger:cam1:State_RBV',  # detector control
-    'pressure_gauge:pressure',  # chamber pressure sensor
-    'portenta:t0', 'portenta:t1',  # temperature sensors
-    'pa0:sample:proposal',  # metadata server aka parrot
-    'image:ratio',  # image analysis ioc
+    "newport1:ysam",
+    # 'newport1:zsam',
+    "mc0:zheavy",
+    "newport1:yawgi",
+    "newport1:rollgi",
+    "newport1:pitchgi",
+    "ims:detx",
+    "ims:dety",
+    "ims:detz",
+    "ims:s1bot",
+    "ims:s1top",
+    "ims:s1hl",
+    "ims:s1hr",
+    "ims:s2bot",
+    "ims:s2top",
+    "ims:s2hl",
+    "ims:s2hr",
+    "ims:s3bot",
+    "ims:s3top",
+    "ims:s3hl",
+    "ims:s3hr",
+    "ims:bsr",
+    "ims:bsz",
+    "ims:dual",
+    "source_cu:shutter",  # uncomment once the source is repaired
+    "source_mo:shutter",
+    "eiger:cam1:State_RBV",  # detector control
+    "pressure_gauge:pressure",  # chamber pressure sensor
+    "portenta:t0",
+    "portenta:t1",  # temperature sensors
+    "pa0:sample:proposal",  # metadata server aka parrot
+    "image:ratio",  # image analysis ioc
 ]
 
 # check that the PVs are reachable before we execute a script:
@@ -54,7 +56,9 @@ for pv in required_pvs:
     if value is None:
         raise ConnectionError(f"PV '{pv}' is not reachable or has no value.")
 
-experiment = ExperimentVariables(required_pvs, data_dir = Path("/home/ws8665-epics/data/"))
+experiment = ExperimentVariables(
+    required_pvs, data_dir=Path("/home/ws8665-epics/data/")
+)
 
 # ensure motor positions are at least checked at the beginning of a measurement
 caput("pa0:config:config_id", 999)
